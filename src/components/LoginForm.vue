2656b8da2c2ff0a96d9bca3c83278ffbcf15b643
Vytautas Morkunas <vytautas.morkunas@panko.lt>
2018-04-27 16:00:21 +0300

    va

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h3>Prisijungimas</h3>
+    <form class="form-control">
+      <label for="email">El. paštas<br />
+        <input id="email" type="email" placeholder="Įveskite el. paštą" />
+      </label><br />
+      <label for="password">Slaptažodis<br />
+        <input id="password" type="password" placeholder="Įveskite slaptažodį" />
+      </label><br />
+      <button type="submit" class="btn btn-primary">Prisijungti</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+      data () {
+        return {
+
+        }
+      }
+  }
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import bootstrap from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(bootstrap);
 
 new Vue({
   el: '#app',
